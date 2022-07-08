c1c533d12f64311480a74854f99a4ea3c23c9fdc
MedAliKrifa <daliolimona@gmail.com>
2022-07-08 11:45:01 +0100

    update on printf

---
+++ get_printf.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * get_printf - get_printf to get function from function.c
+ * @ap: arg
+ * @c: char
+ * Return: char
+ */
+int get_printf(const char c, va_list ap)
+{
+    int i = 0;
+    int j = 0;
+    printf_t type[] = {
+        {'c', pchar},
+        {'s', str},
+        {'%', per}};
+    while (type[i].t != 0)
+    {
+        if (type[i].t == c)
+        {
+            j += type[i].f(ap);
+            return (j);
+        }
+        i++;
+    }
+    return (0);
+}
