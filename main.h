cbbecd15e5fd1462babb1025a01f7080e0ebebdd
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-07-08 02:26:46 +0100

    the header file

--- main.h
+++ main.h
@@ -4,7 +4,21 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <string.h>
-#include <ctype.h>
+#include <unistd.h>
+
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct printf_type
+{
+	char *t;
+	char (*f)(va_list *);
+} printf_t;
 int _printf(const char *format, ...);
-int check (char a, char *s);
+char pchar(va_list ap);
+char str(va_list ap);
+char per(va_list ap);
 #endif
