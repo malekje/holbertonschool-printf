cd6b2c9c272d2cef6380651159eea7f7d391ce63
Malek Mayeh <loukatankionline@gmail.com>
2022-07-08 15:32:24 +0100

    print func

--- _printf.c
+++ _printf.c
@@ -4,42 +4,47 @@
  * @format: is a character string
  * Return: the number of characters printed
  */
-int _printf(const char *format, ...)
+int _printf(const charformat, ...)
 {
 
-int i = 0, j = 0, a = 0;
-va_list ap;
+    int i = 0, j = 0, a = 0;
+    va_list ap;
 
+    if (format == NULL || (strlen(format) == 1 && format[0] == '%'))
+    {
+        return (-1);
+    }
+    va_start(ap, format);
+    while (format[i])
+    {
+        if (format[i] != '%')
+        {
+            putchar(format[i]);
+            j++;
+        }
+        if (format[i] == '%')
+        {
+            a = get_printf(format[i + 1], ap);
+            if (a == -1)
+            {
+                putchar('%');
+                a += 1;
+            }
+            else
+            {
+                j = j + a;
+                i = i + 2;
+                continue;
+            }
 
-
-if (format == NULL || (strlen(format) == 1 && format [0] == '%'))
-{
-return (-1);
-}
-va_start(ap, format);
-while (format[i])
-{
-if (format[i] != '%')
-{
-    putchar(format[i]);
-    j++;
-}
-if (format[i] == '%')
-{
-a = get_printf(format[i + 1], ap);
-if (a > 0)
-j = j + a;
-i = i + 2;
-continue;
-
-if (format[i] == '\0')
-{
-    putchar(format [i]);
-    j++;
-}
-}
-i++;
-}
-va_end (ap);
-return (j);
+            if (format[i + 1] == '\0')
+            {
+                putchar(format[i]);
+                j++;
+            }
+        }
+        i++;
+    }
+    va_end(ap);
+    return (j);
 }
