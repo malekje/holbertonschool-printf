aab7e1913c2fd82501abe1621daf0366e5fff828
Malek Mayeh <loukatankionline@gmail.com>
2022-07-09 23:42:36 +0100

    print func

--- _printf.c
+++ _printf.c
@@ -7,37 +7,41 @@
 int _printf(const char *format, ...)
 {
 
-    int i = 0, j = 0, a = 0;
-    va_list ap;
+	int i = 0, j = 0, a = 0;
+	va_list ap;
 
-    if (format == NULL || (strlen(format) == 1 && format[0] == '%'))
-    {
-        return (-1);
-    }
-    va_start(ap, format);
-    while (format[i])
-    {
-        if (format[i] != '%')
-        {
-            putchar(format[i]);
-            j++;
-        }
-        if (format[i] == '%')
-        {
-            a = get_printf(format[i + 1], ap);
-            if (a > 0)
-                j = j + a;
-            i = i + 2;
-            continue;
-
-            if (format[i] == '\0')
-            {
-                putchar(format[i]);
-                j++;
-            }
-        }
-        i++;
-    }
-    va_end(ap);
-    return (j);
+	if (format == NULL || (strlen(format) == 1 && format[0] == '%'))
+	{
+		return (-1);
+	}
+	va_start(ap, format);
+	while (format && format[i])
+	{
+		if (format[i] != '%')
+		{
+			putchar(format[i]);
+			j++;
+		}
+		if (format[i] == '%' && format[i + 1] != 'K' && format[i + 1] != '!')
+		{
+			a = get_printf(*(format + (i + 1)), ap);
+			if (a != 0)
+				j = j + a;
+			i = i + 2;
+			continue;
+			if (*(format + (i + 1)) == '\0')
+			{
+				putchar(format[i]);
+				j++;
+			}
+		}
+		else if (format[i + 1] == 'K' || format[i + 1] == '!')
+		{
+			putchar(format[i]);
+			j++;
+		}
+		i++;
+	}
+	va_end(ap);
+	return (j);
 }
