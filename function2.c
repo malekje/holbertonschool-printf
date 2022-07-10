f748d3276411cee94a0f09dec79d323c4405aa42
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-07-10 03:16:24 +0100

    FUNC FILE

--- function2.c
+++ function2.c
@@ -151,7 +151,7 @@ int pSTR(va_list arg)
 		s = "(null)";
 	while (s[i] != '\0')
 	{
-		if (s[i - 1] == '\\' && s[i] == 'n')
+		if (s[i] == '\\' && s[i + 1] == 'n')
 		{
 			putchar('x');
 			putchar('0');
