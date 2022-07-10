1bc966b327291014c6b671b8a8b345c49119e627
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-07-10 01:57:57 +0100

    side

---
+++ conversation.c
@@ -0,0 +1,31 @@
+/**
+ * HEXADECIMAL - Unsigned hexadecimal integer (uppercase)
+ * @n: number
+ * Return: length
+ */
+int	HEXADECIMAL(unsigned int n)
+{
+	int c = 0;
+	char a[] = "0123456789ABCDEF";
+
+	if (n >= 16)
+		c += HEXADECIMAL(n / 16);
+	c += putchar(a[n % 16]);
+	return (c);
+}
+
+/**
+ * hexadecimal - change base
+ * @n: the number
+ * Return: length
+ */
+int	hexadecimal(unsigned int n)
+{
+	int c = 0;
+	char a[] = "0123456789abcdef";
+
+	if (n >= 16)
+		c += hexadecimal(n / 16);
+	c += putchar(a[n % 16]);
+	return (c);
+}
